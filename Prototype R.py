4368c9a1cdbd5d73e263ffeadad7498b90a7f6e3
APT-3nma <enmarodri177@gmail.com>
2026-04-02 23:01:43 -0400

    Update Prototype R.py

--- Prototype R.py
+++ Prototype R.py
@@ -4,3 +4,8 @@ import os
 Gemini_API_KEY = os.getenv("######################################")
 Discord_Bot_Token = os.getenv("######################################")
 client = google.genai.Gemini(Gemini_API_KEY)
+Discord_Bot_Token = os.getenv("#########################################################")
+client = google.genai.Gemini(Gemini_API_KEY)
+canvas = Canvas("https://######.instructure.com", "any")
+bot = discord.ext.commands.Bot(command_prefix="/")
+ @bot.command()
