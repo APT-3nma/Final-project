0f8964c0e63f6f1156e2b2d39df93258d2745944
APT-3nma <enmarodri177@gmail.com>
2026-09-04 16:29:17 -0400

    Add files via upload

--- Helios.java
+++ Helios.java
@@ -1,7 +1,8 @@
+package com.helios;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
-import net.dv8ition.jda.api.events.message.MessageReceivedEvent;
-import net.dv8ition.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 
 import dev.langchain4j.model.googleai.GoogleAiGeminiChatModel;
